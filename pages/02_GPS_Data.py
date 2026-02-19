fe8d65566325b9f7e4640d3bbd54a14751359208
moffergeld <mervinoffergeld@gmail.com>
2026-02-19 10:22:03 +0100

    Update 02_GPS_Data.py

--- pages/02_GPS_Data.py
+++ pages/02_GPS_Data.py
@@ -28,9 +28,9 @@ import pandas as pd
 import requests
 import streamlit as st
 
-import pages.Subscripts.session_load_pages
-import pages.Subscripts.acwr_pages
-import pages.Subscripts.ffp_pages
+import pages.Subscripts.session_load_pages as session_load_pages
+import pages.Subscripts.acwr_pages as acwr_pages
+import pages.Subscripts.ffp_pages as ffp_pages
 
 st.set_page_config(page_title="GPS Data", layout="wide")
 
