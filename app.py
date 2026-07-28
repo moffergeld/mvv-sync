697eeda736311c0ccd33a5133cc054a351000379
moffergeld <mervinoffergeld@gmail.com>
2026-07-28 09:07:38 +0200

    Restore premium week report session chart styling

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ TEAM_LOGO = ASSETS_DIR / "Team_Logos" / "MVV Maastricht.png"
 HOME_BG = ASSETS_DIR / "Backgrounds" / "team_page_hero.png"
 
 ACWR_HOME_METRICS = [("total_distance", "ACWR TD")]
-APP_BUILD_STAMP = "MAIN-20260728A"
+APP_BUILD_STAMP = "MAIN-20260728C"
 
 
 def _fallback_consume_login_notice() -> Optional[str]:
